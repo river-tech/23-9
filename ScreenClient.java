36a2b13a64415d82f91521a943bdf5d029024c30
rivertech <ahmobile17022005@gmail.com>
2025-09-28 23:11:17 +0700

    update_fullscrenn_good_qualify

--- ScreenClient.java
+++ ScreenClient.java
@@ -14,21 +14,26 @@ class ScreenPanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g); // double-buffer giúp không bị nháy
+        // if (frame != null) {
+        //     long t1 = System.currentTimeMillis();
+        //     int w = getWidth() - 2 * off;
+        //     System.out.println("Width: " + w);
+        //     int h = getHeight() - 2 * off;
+        //     System.out.println("Height: " + h);
+        //     Image img2 = frame.getScaledInstance(w, h, Image.SCALE_SMOOTH);
+        //     g.drawImage(img2, off, off, getWidth() - off, getHeight() - off,
+        //             0, 0, w, h, null);
+        //     long t2 = System.currentTimeMillis();
+        //     System.out.println("Draw time: " + (t2 - t1) + " ms");
+        // }
         if (frame != null) {
-            // int w = getWidth() - 2 * off;
-            // System.out.println("Width: " + w);
-            // int h = getHeight() - 2 * off;
-            // System.out.println("Height: " + h);
-            // Image img2 = frame.getScaledInstance(w, h, Image.SCALE_SMOOTH);
-            // g.drawImage(img2, off, off, getWidth() - off, getHeight() - off,
-            //         0, 0, w, h, null);
-             if (frame != null) {
-            long t1 = System.currentTimeMillis();
-        g.drawImage(frame, 0, 0, this);  // vẽ nguyên ảnh server gửi
+        long t1 = System.currentTimeMillis();
+      int w = getWidth();
+    int h = getHeight();
+    g.drawImage(frame, 0, 0, w, h, null);
         long t2 = System.currentTimeMillis();
         System.out.println("Draw time: " + (t2 - t1) + " ms");
     }
-        }
     }
 }
 
@@ -62,26 +67,26 @@ public class ScreenClient extends JFrame {
         try {
             DataInputStream bis = new DataInputStream(soc.getInputStream());
             long frameCount = 0;
-            long startTime = System.nanoTime();
+            long startTime = System.currentTimeMillis();
 
             while (true) {
                 int n = bis.readInt();            // số byte ảnh
                 byte tmp[] = bis.readNBytes(n);   // đọc dữ liệu ảnh
 
-                long t0 = System.nanoTime();
+                long t0 = System.currentTimeMillis();
                 BufferedImage img = ImageIO.read(new ByteArrayInputStream(tmp)); // decode JPEG
-                long decodeTime = System.nanoTime() - t0;
+                long decodeTime = System.currentTimeMillis() - t0;
 
                 panel.frame = img;  // lưu frame mới
                 frameCount++;
 
             
                 if (frameCount % 100 == 0) {
-                    long elapsed = (System.nanoTime() - startTime) / 1000000;
+                    long elapsed = (System.currentTimeMillis() - startTime) / 1000;
                     double fps = (double) frameCount / elapsed;
                     System.out.println("[CLIENT] Frames=" + frameCount +
                             " | size=" + (tmp.length / 1024) + " KB" +
-                                " | decode=" + decodeTime + " ns" +
+                                " | decode=" + decodeTime + " ms" +
                             " | FPS≈" + String.format("%.2f", fps));
                 }
 
--- ScreenServer.java
+++ ScreenServer.java
@@ -9,6 +9,8 @@ import java.net.Socket;
 import javax.imageio.ImageIO;
 import java.awt.Image;
 import java.awt.Graphics2D;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.awt.Dimension;
 
 public class ScreenServer {
     public static void main(String[] args) {
@@ -16,16 +18,17 @@ public class ScreenServer {
     }
 
     public ScreenServer() {
+        System.out.println("ScreenServer");
         Screen s = new Screen();
         s.start();
-
         try (ServerSocket server = new ServerSocket(2345)) {
             System.out.println("[SERVER] Listening on port 2345...");
             while (true) {
                 Socket soc = server.accept();
-                System.out.println("[SERVER] Client connected: " + soc.getInetAddress());
-                ScreenProcessing sp = new ScreenProcessing(soc);
-                sp.start();
+                // System.out.println("[SERVER] Client connected: " + soc.getInetAddress());
+                // ScreenProcessing sp = new ScreenProcessing(soc);
+                // sp.start();
+                ClientManager.addClient(soc);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -37,32 +40,39 @@ class Screen extends Thread {
     static byte[] tmp;
     static int count = 0;
     private long startTime = System.currentTimeMillis();
-
     public void run() {
+        System.out.println("Screen");
         try {
             Robot r = new Robot();
+            
             Rectangle capture = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
 
             while (true) {
                 long t0 = System.currentTimeMillis();
                	BufferedImage img = r.createScreenCapture(capture);
-
                 long t1 = System.currentTimeMillis();
 
-				int targetW = 1080, targetH = 600;
-				Image scaled = img.getScaledInstance(targetW, targetH, Image.SCALE_FAST);
+				// int targetW = 1080, targetH = 600;
+                Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+                int targetW = (int) screenSize.getWidth();
+                int targetH = (int) screenSize.getHeight();
+				Image scaled = img.getScaledInstance(targetW, targetH, Image.SCALE_DEFAULT);
+
 				BufferedImage down = new BufferedImage(targetW, targetH, BufferedImage.TYPE_INT_RGB);
 				Graphics2D g2d = down.createGraphics();
 				g2d.drawImage(scaled, 0, 0, targetW, targetH, null);
 				g2d.dispose();
+
                 long t2 = System.currentTimeMillis();
 
 				ByteArrayOutputStream bos = new ByteArrayOutputStream();
 				ImageIO.write(down, "jpg", bos);
 				bos.flush();
 				tmp = bos.toByteArray();
+                
                  long t3 = System.currentTimeMillis();
 				count++;
+                ClientManager.broadcast(tmp);
                 long encodeTime = t3 - t0;
 
                if (count % 100 == 0) {
@@ -85,29 +95,56 @@ class Screen extends Thread {
     }
 }
 
-class ScreenProcessing extends Thread {
-    Socket soc;
-    int countNow;
+// class ScreenProcessing extends Thread {
+//     Socket soc;
+//     int countNow;
+  
+//     public ScreenProcessing(Socket soc) {
+//         this.soc = soc;
+//     }
 
-    public ScreenProcessing(Socket soc) {
-        this.soc = soc;
+//     public void run() {
+//           System.out.println("ScreenProcessing");
+//         try (DataOutputStream output = new DataOutputStream(soc.getOutputStream())) {
+//             while (true) {
+//                 if (countNow == Screen.count || Screen.tmp == null) {
+//                     Thread.sleep(5);
+//                     continue;
+//                 }
+//                 byte[] tmp = Screen.tmp.clone();
+//                 countNow = Screen.count;
+//                 output.writeInt(tmp.length);
+//                 output.write(tmp);
+//                 output.flush();
+//             }
+//         } catch (Exception e) {
+//             System.out.println("[SERVER] Client disconnected.");
+//         }
+//     }
+// }
+
+class ClientManager {
+    static CopyOnWriteArrayList<DataOutputStream> clients = new CopyOnWriteArrayList<>();
+    public static void addClient(Socket soc) {
+        System.out.println("ClientManager");
+        try {
+            clients.add(new DataOutputStream(soc.getOutputStream()));
+            System.out.println("[SERVER] Added client: " + soc.getInetAddress());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void run() {
-        try (DataOutputStream output = new DataOutputStream(soc.getOutputStream())) {
-            while (true) {
-                if (countNow == Screen.count || Screen.tmp == null) {
-                    Thread.sleep(5);
-                    continue;
-                }
-                byte[] tmp = Screen.tmp.clone();
-                countNow = Screen.count;
-                output.writeInt(tmp.length);
-                output.write(tmp);
-                output.flush();
+    public static void broadcast(byte[] frame) {
+        for (DataOutputStream out : clients) {
+            try {
+                out.writeInt(frame.length);
+                out.write(frame);
+                out.flush();
+            } catch (Exception e) {
+                clients.remove(out);
+                System.out.println("[SERVER] Removed disconnected client.");
             }
-        } catch (Exception e) {
-            System.out.println("[SERVER] Client disconnected.");
         }
     }
 }
